de04f92ca94fac72fec0b82ba6ea18f541909c69
Ambroise <ambrohdg07@gmail.com*>
2026-04-26 00:38:39 +0100

    Préparation finale des tables

--- orientation/settings.py
+++ orientation/settings.py
@@ -78,13 +78,16 @@ WSGI_APPLICATION = 'orientation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
+import os
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
