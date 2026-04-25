dcf9d6edcbae028bd1f9a4612e2d68dfcb050932
Ambroise <ambroisehoundagnanme@gmail.com>
2026-04-25 23:34:22 +0100

    Update settings.py

--- orientation/settings.py
+++ orientation/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7tfjf1ubruwpnz80j+%14$m%)n%*x2fra1a&+tlevr*6piu3d9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
